fefd50b5da45ac4c7f3b8082da246e5de50c689c
yarasa27 <elcan.abbasoff27@gmail.com>
2024-10-05 15:21:32 +0400

    open/close modal

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const messages = document.querySelector('.messages');
 const message = messages.querySelectorAll('.message');
 const messageSearch = document.querySelector('#message-search');
 
+// THEME
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
+
 // ================ SIDEBAR =================
 
 // remove acitve class from all menu items
@@ -54,3 +58,23 @@ messagesNotification.addEventListener('click', () => {
         messages.style.boxShadow = 'none';
     }, 2000);
 })
+
+// THEME/DISPLAY CUSTOMIZATION
+
+//  opens modal
+const openThemeModal = () => {
+    themeModal.style.display = 'grid';
+}
+
+// closes modal
+const closeThemeModal = (e) => {
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display = 'none';
+    }
+}
+
+
+// close modal
+themeModal.addEventListener('click', closeThemeModal);
+
+theme.addEventListener('click', openThemeModal);
